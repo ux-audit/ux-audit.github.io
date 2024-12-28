9ff946626bd171b09ec58809e56fd3b302479916
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-12-28 16:57:46 +0100

    dev: bad checkbox

--- exemples/loi-de-fitts/taille.md
+++ exemples/loi-de-fitts/taille.md
@@ -3,3 +3,12 @@ outline: deep
 ---
 
 # Taille
+
+<script setup lang="ts">
+import BadCHeckBox from "../utils/BadCHeckBox.vue";
+
+</script>
+
+❌ Bouton checkbox trop petit
+
+<BadCHeckBox />
---
+++ exemples/utils/BadCheckBox.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useData } from "vitepress";
+
+const { isDark } = useData();
+</script>
+
+<template>
+  <label class="flex gap-2 rounded-md border border-[--vp-c-gray-1] p-2">
+    <input type="checkbox" v-model="isDark" />
+    <span>Activer le dark thème</span>
+  </label>
+</template>
